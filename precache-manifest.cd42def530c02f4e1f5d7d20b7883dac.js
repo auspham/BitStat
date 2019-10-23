d8294a0f1eddc0fca903ef9318d984a03dcd56a8
Austin <rockmanvnx6@gmail.com>
2019-10-23 22:35:26 +1100

    Updates

---
+++ precache-manifest.cd42def530c02f4e1f5d7d20b7883dac.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a7db89dc5ecfc8481d48d08e36e0b5",
+    "url": "/rockmanvnx6/BitStat/index.html"
+  },
+  {
+    "revision": "582b1a6f4bc9c9069dea",
+    "url": "/rockmanvnx6/BitStat/static/css/2.045a154d.chunk.css"
+  },
+  {
+    "revision": "807e44176eaa424e5772",
+    "url": "/rockmanvnx6/BitStat/static/css/main.688134e4.chunk.css"
+  },
+  {
+    "revision": "582b1a6f4bc9c9069dea",
+    "url": "/rockmanvnx6/BitStat/static/js/2.e3592a89.chunk.js"
+  },
+  {
+    "revision": "807e44176eaa424e5772",
+    "url": "/rockmanvnx6/BitStat/static/js/main.031b13a0.chunk.js"
+  },
+  {
+    "revision": "a472469842edb6ef1b0f",
+    "url": "/rockmanvnx6/BitStat/static/js/runtime-main.980fa239.js"
+  },
+  {
+    "revision": "e1bc50542543d9f1ae343a786293000a",
+    "url": "/rockmanvnx6/BitStat/static/media/16440123.e1bc5054.jpeg"
+  },
+  {
+    "revision": "67ca8b463f187e64d5fe4c37108599e4",
+    "url": "/rockmanvnx6/BitStat/static/media/Google-Cloud-Platform-grey.67ca8b46.png"
+  },
+  {
+    "revision": "5630d3bd872d0586add3d64c9e688095",
+    "url": "/rockmanvnx6/BitStat/static/media/cloud-computing.5630d3bd.svg"
+  },
+  {
+    "revision": "b0e48af5354fcaaf6fd51e2e12acd212",
+    "url": "/rockmanvnx6/BitStat/static/media/database.b0e48af5.svg"
+  },
+  {
+    "revision": "5b173b7f5f012c20774588f918f35880",
+    "url": "/rockmanvnx6/BitStat/static/media/mobile.5b173b7f.png"
+  },
+  {
+    "revision": "8c9d4258c29e89cb404881948ec3f7c5",
+    "url": "/rockmanvnx6/BitStat/static/media/partner-logos-grey-amazonaws.8c9d4258.png"
+  },
+  {
+    "revision": "929e9ce48782abfd45043a78e3804384",
+    "url": "/rockmanvnx6/BitStat/static/media/reactjs-card.929e9ce4.png"
+  },
+  {
+    "revision": "a47d5124e88532695ba37fcfa9b5cf19",
+    "url": "/rockmanvnx6/BitStat/static/media/sadgery.a47d5124.png"
+  },
+  {
+    "revision": "862f55a9bea5a5bbc9af963263467e40",
+    "url": "/rockmanvnx6/BitStat/static/media/server-less.862f55a9.svg"
+  },
+  {
+    "revision": "e73c205ecbc366a6bd0de1fd008c8d01",
+    "url": "/rockmanvnx6/BitStat/static/media/sprite_60fps.e73c205e.svg"
+  },
+  {
+    "revision": "e13d2e46e5de9034ece4bbf0246b8467",
+    "url": "/rockmanvnx6/BitStat/static/media/wave-bottom.e13d2e46.svg"
+  }
+]);
